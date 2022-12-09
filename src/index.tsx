ac68d0b35691ea6e7d2847a90be011c6404460b0
Khizri Makhmudov <khizrim@khizrim.ru>
2022-12-09 22:29:13 +0300

    feat: add eslint and prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-import App from "./components/app";
+import App from './components/app';
 
-import "./index.css";
+import './index.css';
 
-createRoot(document.getElementById("root") as HTMLElement).render(
-  <StrictMode>
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>,
 );
